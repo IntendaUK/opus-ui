dd0bba9748ff2cf2780d191d21f033bb63a43341
Shaun <skichenbrand@gmail.com>
2024-06-06 16:02:25 +0200

    bug #6: Implemented style flattening to support older browsers

--- src/system/wrapper/helpers/styleTags.js
+++ src/system/wrapper/helpers/styleTags.js
@@ -1,3 +1,5 @@
+const toBeNestedIndicator = '$toBeNested$';
+
 export const removeStyleTag = id => {
 	const el = document.getElementById('style-' + id);
 	if (!el || !el.parentNode)
@@ -6,29 +8,71 @@ export const removeStyleTag = id => {
 	el.parentNode.removeChild(el);
 };
 
-const buildCss = (id, styles, isOuter = false) => {
-	let res = '';
+const recurseCss = (parentSelector, resultObj, cssObj) => {
+	Object.entries(cssObj).forEach(([k, v]) => {
+		const type = typeof(v);
 
-	if (isOuter) {
-		res = `[id="${id}"]${Object.keys(styles)[0]} {`;
-
-		styles = Object.values(styles)[0];
-	}
-
-	const entries = Object.entries(styles);
-	entries.forEach(([k, v]) => {
-		const typeV = typeof(v);
-
-		if (v === null || v === undefined)
+		if (v === undefined || v === null)
 			return;
+		else if (type === 'object') {
+			let childSelector = parentSelector;
+			if (k[0] === '&')
+				childSelector += k.substr(1);
+			else if (k.indexOf('@media') === 0)
+				childSelector = `${k}${toBeNestedIndicator}${parentSelector}`;
+			else if (parentSelector === '')
+				childSelector += k;
+			else
+				childSelector += ` ${k}`;
 
-		if (typeV === 'object')
-			res += `${k} { ${buildCss(id, v)}`;
-		else
-			res += `${k}: ${v};`;
+			recurseCss(childSelector, resultObj, v);
+
+			return;
+		}
+
+		let entry = resultObj[parentSelector];
+		if (!entry) {
+			entry = {};
+
+			resultObj[parentSelector] = entry;
+		}
+
+		entry[k] = v;
 	});
+};
 
-	res = res + '}';
+/* eslint-disable-next-line max-lines-per-function */
+const buildCss = (parentId, cssObj) => {
+	const resultObj = {};
+
+	const selectorPrefix = `[id="${parentId}"]`;
+
+	recurseCss('', resultObj, cssObj);
+
+	const res = Object.entries(resultObj)
+		.map(([k, v]) => {
+			if (k.includes(toBeNestedIndicator)) {
+				const [selectorMedia, selectorInner] = k.split(toBeNestedIndicator);
+
+				const attributes = Object.entries(v)
+					.map(([ik, iv]) => {
+						return `\t\t${ik}: ${iv};`;
+					})
+					.join('\r\n');
+
+				/* eslint-disable-next-line max-len */
+				return `${selectorMedia} {\r\n\t${selectorPrefix}${selectorInner} {\r\n${attributes}\r\n\t}\r\n}`;
+			}
+
+			const attr = Object.entries(v)
+				.map(([ik, iv]) => {
+					return `\t${ik}: ${iv};`;
+				})
+				.join('\r\n');
+
+			return `${selectorPrefix}${k} {\r\n${attr}\r\n}`;
+		})
+		.join('\r\n\r\n');
 
 	return res;
 };
