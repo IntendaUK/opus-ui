3c07c2b8b8c75ba7a1b9d3284e047c8c35d8b7de
Shaun <skichenbrand@gmail.com>
2025-02-04 18:45:27 +0200

    bug #22: fixed themes not applying to dashboards after hot reload

--- src/system/managers/reloadManager.js
+++ src/system/managers/reloadManager.js
@@ -2,8 +2,12 @@ import { getComponentIdsForPath, forceRemount } from './stateManager';
 import { getFilteredParentList } from './scopeManager';
 import { clearTraitCache } from './traitManager/getTrait';
 import { setMdaAtPath } from '../../components/scriptRunner/actions/getMda/getMda';
+import applyThemesToMdaPackage from '../../app/components/helpers/applyThemesToMdaPackage';
 
 export const reloadComponentsFromPath = (path, newMda) => {
+	//This has a side-effect. It modifies newMda in place
+	applyThemesToMdaPackage(newMda);
+
 	clearTraitCache();
 
 	const type = path.indexOf('blueprint/') === 0 ? 'blueprint' : 'dashboard';
